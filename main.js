0e594e34cf300d9330bd07f46fe627e0e8ef0f14
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-28 20:32:17 +0200

    cambio sistema creazione partecipanti

--- main.js
+++ main.js
@@ -6,60 +6,65 @@ let active = document.querySelectorAll('.n-players');
 
 //creazione cards
 //1 player
-for (let index = 0; index < card1.length; index++) {
-    document.getElementById('p1-card').innerHTML += `<div class="Number1">${card1[index]}</div>`;
-}
+
 //2player
-for (let index = 0; index < card1.length; index++) {
-    document.getElementById('p1-2-card').innerHTML += `<div class="Number2">${card1[index]}</div>`;
-}
-for (let index = 0; index < card2.length; index++) {
-    document.getElementById('p2-card').innerHTML += `<div class="Number2">${card2[index]}</div>`;
-}
+
 //3player
-for (let index = 0; index < card1.length; index++) {
-    document.getElementById('p1-3-card').innerHTML += `<div class="Number1">${card1[index]}</div>`;
-}
-for (let index = 0; index < card2.length; index++) {
-    document.getElementById('p2-3-card').innerHTML += `<div class="Number2">${card2[index]}</div>`;
-}
-for (let index = 0; index < card3.length; index++) {
-    document.getElementById('p3-card').innerHTML += `<div class="Number3">${card3[index]}</div>`;
-}
+
 //4player
-for (let index = 0; index < card1.length; index++) {
-    document.getElementById('p1-4-card').innerHTML += `<div class="Number1">${card1[index]}</div>`;
-}
-for (let index = 0; index < card2.length; index++) {
-    document.getElementById('p2-4-card').innerHTML += `<div class="Number2">${card2[index]}</div>`;
-}
-for (let index = 0; index < card3.length; index++) {
-    document.getElementById('p3-4-card').innerHTML += `<div class="Number3">${card3[index]}</div>`;
-}
-for (let index = 0; index < card4.length; index++) {
-    document.getElementById('p4-card').innerHTML += `<div class="Number4">${card4[index]}</div>`;
-}
+
+
 
 //scelta del numero partecipanti
 document.getElementById('play1').addEventListener('click', function () {
     active[0].classList.add('active');
     document.getElementById('player-selection').style.display = 'none';
     document.getElementById('confirm').style.display = 'block';
+    for (let index = 0; index < card1.length; index++) {
+        document.getElementById('p1-card').innerHTML += `<div class="Number1">${card1[index]}</div>`;
+    }
 })
 document.getElementById('play2').addEventListener('click', function () {
     active[1].classList.add('active');
     document.getElementById('player-selection').style.display = 'none';
     document.getElementById('confirm').style.display = 'block';
+    for (let index = 0; index < card1.length; index++) {
+        document.getElementById('p1-2-card').innerHTML += `<div class="Number2">${card1[index]}</div>`;
+    }
+    for (let index = 0; index < card2.length; index++) {
+        document.getElementById('p2-card').innerHTML += `<div class="Number2">${card2[index]}</div>`;
+    }
 })
 document.getElementById('play3').addEventListener('click', function () {
     active[2].classList.add('active');
     document.getElementById('player-selection').style.display = 'none';
     document.getElementById('confirm').style.display = 'block';
+    for (let index = 0; index < card1.length; index++) {
+        document.getElementById('p1-3-card').innerHTML += `<div class="Number1">${card1[index]}</div>`;
+    }
+    for (let index = 0; index < card2.length; index++) {
+        document.getElementById('p2-3-card').innerHTML += `<div class="Number2">${card2[index]}</div>`;
+    }
+    for (let index = 0; index < card3.length; index++) {
+        document.getElementById('p3-card').innerHTML += `<div class="Number3">${card3[index]}</div>`;
+    }
 })
 document.getElementById('play4').addEventListener('click', function () {
     active[3].classList.add('active');
     document.getElementById('player-selection').style.display = 'none';
     document.getElementById('confirm').style.display = 'block';
+    for (let index = 0; index < card1.length; index++) {
+        document.getElementById('p1-4-card').innerHTML += `<div class="Number1">${card1[index]}</div>`;
+    }
+    for (let index = 0; index < card2.length; index++) {
+        document.getElementById('p2-4-card').innerHTML += `<div class="Number2">${card2[index]}</div>`;
+    }
+    for (let index = 0; index < card3.length; index++) {
+        document.getElementById('p3-4-card').innerHTML += `<div class="Number3">${card3[index]}</div>`;
+    }
+    for (let index = 0; index < card4.length; index++) {
+        document.getElementById('p4-card').innerHTML += `<div class="Number4">${card4[index]}</div>`;
+    }
 })
 
 //scelta nomi
@@ -172,12 +177,16 @@ document.getElementById('begin').addEventListener('click', function () {
 //sistema conteggio numeri
 let TableCount = [];
 let NewNum = 0;
+let status1=0;
+let status2=0;
+let status3=0;
+let status4=0;
 document.getElementById('extract').addEventListener('click', function () {
     let compare = document.querySelectorAll('.cpu-number');
     let check1 = document.querySelectorAll('.Number1');
     let check2 = document.querySelectorAll('.Number2');
     let check3 = document.querySelectorAll('.Number3');
-    let check4 = document.querySelectorAll('.Number4');
+    let check4 = document.querySelectorAll('.Number4');  
     let Here = true;
     while (Here) {
         const a = parseInt(Math.random() * (51 - 1) + 1);
@@ -188,29 +197,51 @@ document.getElementById('extract').addEventListener('click', function () {
             for (let index = 0; index < compare.length; index++) {
                 if (a == compare[index].innerHTML) {
                     compare[index].classList.add('checked');
+                    
                 }
             }
+
             for (let index = 0; index < check1.length; index++) {
                 if (a == check1[index].innerHTML) {
                     check1[index].classList.add('had');
+                    status1+=1;
+                    console.log('status 1',status1);
+                    
                 }
             }
+
             for (let index = 0; index < check2.length; index++) {
                 if (a == check2[index].innerHTML) {
                     check2[index].classList.add('had');
+                    status2+=1;
+                    console.log('status 2',status2);   
                 }
             }
+
             for (let index = 0; index < check3.length; index++) {
                 if (a == check3[index].innerHTML) {
                     check3[index].classList.add('had');
+                    status3+=1;
+                    console.log('status 3',status3);   
                 }
             }
+
             for (let index = 0; index < check4.length; index++) {
                 if (a == check4[index].innerHTML) {
                     check4[index].classList.add('had');
+                    status4+=1;
+                    console.log('status 4',status4);
                 }
             }
         }
+    }if(status1==10){
+        alert('giocatore 1 ha vinto');
+    }else if(status2==10){
+        alert('giocatore 2 ha vinto');
+    }else if(status3==10){
+        alert('giocatore 3 havinto');
+    }else if(status4==10){
+        alert('giocatore 4 ha vinto');
     }
 })
 
